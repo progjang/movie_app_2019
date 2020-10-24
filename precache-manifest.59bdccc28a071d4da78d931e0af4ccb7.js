0384ac6c1b3f87a534f3dd0baccaa7abaf907908
PJ <wooritobi@gmail.com>
2020-10-25 00:35:23 +0900

    Updates

---
+++ precache-manifest.59bdccc28a071d4da78d931e0af4ccb7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "489ed6445ff02c673db0eda08c7f2379",
+    "url": "/movie_app_2019/index.html"
+  },
+  {
+    "revision": "e7a3ec5922dcaa38b90a",
+    "url": "/movie_app_2019/static/css/main.ad07565c.chunk.css"
+  },
+  {
+    "revision": "dc0ff1a6bb30c86ecf79",
+    "url": "/movie_app_2019/static/js/2.fe4db541.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/movie_app_2019/static/js/2.fe4db541.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7a3ec5922dcaa38b90a",
+    "url": "/movie_app_2019/static/js/main.7c74787b.chunk.js"
+  },
+  {
+    "revision": "059a9ab2adfa282ae316",
+    "url": "/movie_app_2019/static/js/runtime-main.12568eff.js"
+  }
+]);
